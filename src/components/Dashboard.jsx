4bdd72177430772fee3f572d9d16374675b6f3c7
Navaneeth Suresh <navaneeth22yt@gmail.com>
2023-01-05 13:50:49 +0530

    dashboard changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -133,160 +133,164 @@ const Dashboard = () => {
     }, [moisturePercentage]);
 
     // trigger when to turn on and turn off based on threshold
-    useEffect(() => {
-        if (currentMoisture > 1800) {
-            turnOn();
-        } else {
-            turnOff();
-        }
-    }, [currentMoisture]);
+    // useEffect(() => {
+    //     if (wateringSystemMode === 'MANUAL') {
+    //     } else if (wateringSystemMode === 'AUTOMATIC')){
+    //     if (currentMoisture > 1800) {
+    //         turnOn();
+    //     } else {
+    //         turnOff();
+    //     }
+    // }
 
-    //function to call temperature and humidity
-    const getTemp = () => {
-        const options = {
-            method: 'GET',
-            url: 'https://api.openweathermap.org/data/2.5/weather',
-            params: { lat: '13', lon: '77.5', appid: '406b154331868aa69ddc3dd64454c8c6' }
-        };
+}, [currentMoisture, wateringSystemMode]);
 
-        axios.request(options).then(function (response) {
-            let temp = response.data.main.temp - 273.15;
-            temp = temp.toFixed(2);
-            setTemperature(temp);
-            console.log(response.data);
-        }).catch(function (error) {
-            console.error(error);
-        });
-    }
+//function to call temperature and humidity
+const getTemp = () => {
+    const options = {
+        method: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/weather',
+        params: { lat: '13', lon: '77.5', appid: '406b154331868aa69ddc3dd64454c8c6' }
+    };
 
-    return (
-        <>
-            <h1 class="font-bold text-3xl mt-5 mx-3 justify-center items-center text-center">Plant Monitoring System</h1>
-            <div className="mt-4 mx-5 grid md:grid-cols-5">
-                {/* first card */}
-                <div className="card bg-base-100 shadow-xl col-span-2 m-2">
-                    <div className="card-body">
-                        <h2 className="card-title justify-center items-center text-center">Watering system is in {wateringSystemMode} mode</h2>
+    axios.request(options).then(function (response) {
+        let temp = response.data.main.temp - 273.15;
+        temp = temp.toFixed(2);
+        setTemperature(temp);
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
 
-                        {/* inner card for manual mode */}
-                        <div className="card bg-slate-200 text-base-content">
-                            <div className="card-body items-center text-center">
-                                <h2 className="card-title">Manual Mode</h2>
-                                <div className="card-actions justify-center items-center text-center">
-                                    <button className={`btn btn-success btn-${btnState}`} onClick={turnOn}>Turn ON</button>
-                                    <button className={`btn btn-error btn-${btnState}`} onClick={turnOff}>Turn OFF</button>
-                                </div>
+return (
+    <>
+        <h1 class="font-bold text-3xl mt-5 mx-3 justify-center items-center text-center">Plant Monitoring System</h1>
+        <div className="mt-4 mx-5 grid md:grid-cols-5">
+            {/* first card */}
+            <div className="card bg-base-100 shadow-xl col-span-2 m-2">
+                <div className="card-body">
+                    <h2 className="card-title justify-center items-center text-center">Watering system is in {wateringSystemMode} mode</h2>
+
+                    {/* inner card for manual mode */}
+                    <div className="card bg-slate-200 text-base-content">
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Manual Mode</h2>
+                            <div className="card-actions justify-center items-center text-center">
+                                <button className={`btn btn-success btn-${btnState}`} onClick={turnOn}>Turn ON</button>
+                                <button className={`btn btn-error btn-${btnState}`} onClick={turnOff}>Turn OFF</button>
                             </div>
                         </div>
-
-                        <div className="text-xl justify-center items-center text-center gap-4">
-                            <div>Watering system is now <b className={`${textState}`}><i>{wateringStatus}</i></b></div>
-                        </div>
-
-                        {/* inner card for changing from manual to auto */}
-                        <div className="card bg-slate-200 text-base-content">
-                            <div className="card-body items-center text-center">
-                                <h2 className="card-title">Switch between manual and automatic mode</h2>
-                                <div className="card-actions justify-center items-center text-center">
-                                    <button className="btn btn-info" onClick={() => setWateringSystemMode('MANUAL')}>Manual Mode</button>
-                                    <button className="btn btn-accent" onClick={() => setWateringSystemMode('AUTOMATIC')}>Automatic Mode</button>
-                                </div>
-                            </div>
-                        </div>
-
-
                     </div>
-                </div>
 
-                {/* second card */}
-                <div className="card bg-base-100 shadow-xl col-span-3  m-2">
-                    <div className="card-body">
-                        <h1 className="card-title justify-center">Plant Information</h1>
-                        <div className="divider"></div>
-
-                        {/* progress or lifecycle */}
-                        <ul className="steps steps-vertical lg:steps-horizontal">
-                            <li className="step step-success">Seedling</li>
-                            <li className="step step-success">Vegetative</li>
-                            <li className="step">Flowering</li>
-                            <li className="step">Fruit development</li>
-                            <li className="step">Harvest</li>
-                        </ul>
-
-                        <div className="divider"></div>
-
-                        <div className="grid grid-cols-3">
-
-                            {/* current soil moisture level */}
-                            <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
-                                <div className="card-body">
-                                    <h2 className="card-title">Current Soil Moisture</h2>
-                                    <div className="card-actions">
-                                        <h3 class={`font-bold leading-tight text-2xl mt-0 mb-2 justify-center items-center text-center text-${moisturePercentageColor}`}>{moisturePercentage} %</h3>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* duration */}
-                            <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
-                                <div className="card-body">
-                                    <h2 className="card-title">Irrigation Duration</h2>
-                                    <div className="card-actions">
-                                        <h3 class="font-bold leading-tight text-2xl mt-0 mb-2 justify-center items-center text-center text-warning">{irrigationDuration} mins</h3>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* ltrs */}
-                            <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
-                                <div className="card-body">
-                                    <h2 className="card-title">Irrigation Quantity</h2>
-                                    <div className="card-actions">
-                                        <h3 class="font-bold leading-tight text-2xl mt-0 mb-2 justify-center items-center text-center text-info">{irrigationQuantity} ltrs</h3>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* current soil moisture reading */}
-                            <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
-                                <div className="card-body">
-                                    <h2 className="card-title">Current Soil Moisture Reading</h2>
-                                    <div className="card-actions">
-                                        <h3 class={`font-bold leading-tight text-2xl mt-0 mb-2 justify-center items-center text-center`}>{currentMoisture} V</h3>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* current temperature */}
-                            <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
-                                <div className="card-body">
-                                    <h2 className="card-title" onClick={getTemp}>Temperature</h2>
-                                    <div className="card-actions">
-                                        <h3 class={`font-bold leading-tight text-2xl mt-0 mb-2 justify-center items-center text-center`}>{temperature} °C</h3>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* current humidity */}
-                            <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
-                                <div className="card-body">
-                                    <h2 className="card-title">Rainfall</h2>
-                                    <div className="card-actions">
-                                        <h3 class={`font-bold leading-tight text-2xl mt-0 mb-2 justify-center items-center text-center`}>0 %</h3>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-
-
+                    <div className="text-xl justify-center items-center text-center gap-4">
+                        <div>Watering system is now <b className={`${textState}`}><i>{wateringStatus}</i></b></div>
                     </div>
-                </div>
 
+                    {/* inner card for changing from manual to auto */}
+                    <div className="card bg-slate-200 text-base-content">
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">Switch between manual and automatic mode</h2>
+                            <div className="card-actions justify-center items-center text-center">
+                                <button className="btn btn-info" onClick={() => setWateringSystemMode('MANUAL')}>Manual Mode</button>
+                                <button className="btn btn-accent" onClick={() => setWateringSystemMode('AUTOMATIC')}>Automatic Mode</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
-        </>
-    );
+
+            {/* second card */}
+            <div className="card bg-base-100 shadow-xl col-span-3  m-2">
+                <div className="card-body">
+                    <h1 className="card-title justify-center">Plant Information</h1>
+                    <div className="divider"></div>
+
+                    {/* progress or lifecycle */}
+                    <ul className="steps steps-vertical lg:steps-horizontal">
+                        <li className="step step-success">Seedling</li>
+                        <li className="step step-success">Vegetative</li>
+                        <li className="step">Flowering</li>
+                        <li className="step">Fruit development</li>
+                        <li className="step">Harvest</li>
+                    </ul>
+
+                    <div className="divider"></div>
+
+                    <div className="grid grid-cols-3">
+
+                        {/* current soil moisture level */}
+                        <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
+                            <div className="card-body">
+                                <h2 className="card-title">Current Soil Moisture</h2>
+                                <div className="card-actions">
+                                    <div class={`stat-value text-center text-${moisturePercentageColor}`}>{moisturePercentage} %</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* duration */}
+                        <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
+                            <div className="card-body">
+                                <h2 className="card-title">Irrigation Duration</h2>
+                                <div className="card-actions">
+                                    <div class="stat-value text-warning">{irrigationDuration} mins</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* ltrs */}
+                        <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
+                            <div className="card-body">
+                                <h2 className="card-title">Irrigation Quantity</h2>
+                                <div className="card-actions">
+                                    <h3 class="stat-value text-info">{irrigationQuantity} ltrs</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* current soil moisture reading */}
+                        <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
+                            <div className="card-body">
+                                <h2 className="card-title">Current Soil Moisture Reading</h2>
+                                <div className="card-actions">
+                                    <h3 class="stat-value">{currentMoisture} V</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* current temperature */}
+                        <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
+                            <div className="card-body">
+                                <h2 className="card-title" onClick={getTemp}>Temperature</h2>
+                                <div className="card-actions">
+                                    <h3 class="stat-value">{temperature} °C</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* current humidity */}
+                        <div className="card bg-base-100 shadow-xl m-2 lg:col-span-1 col-span-3">
+                            <div className="card-body">
+                                <h2 className="card-title">Rainfall</h2>
+                                <div className="card-actions">
+                                    <h3 class="stat-value ">0 %</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </div>
+
+        </div>
+    </>
+);
 }
 
 export default Dashboard;
