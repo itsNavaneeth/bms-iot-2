2f629d877138b2af87b78928f1c94af2915f27f3
tortsBsus <meganraj251@gmail.com>
2023-01-06 12:03:44 +0530

    dynamic graph commit

--- src/components/GraphBoard.jsx
+++ src/components/GraphBoard.jsx
@@ -10,7 +10,7 @@ const GraphBoard = () => {
   const [link, setLink] = useState(
     "https://api.thingspeak.com/channels/1985902/feeds.json?api_key=KKDDQDQZP8VLRQWR&"
   );
-  const [timeRange, setTimeRange] = useState(90);
+  const [timeRange, setTimeRange] = useState(30);
   const [soilData, setSoilData] = useState([]);
   const [tempData, setTempData] = useState([]);
   const [rainData, setRainData] = useState([]);
@@ -21,6 +21,7 @@ const GraphBoard = () => {
   const [soilcheckox, setSoilCheckbox] = useState(false);
   const [tempcheckox, setTempCheckbox] = useState(false);
   const [raincheckox, setRainCheckbox] = useState(false);
+  let value;
 
   //fetches historical data from thingspeak for graphs
   useEffect(() => {
@@ -46,7 +47,7 @@ const GraphBoard = () => {
       });
 
     const interval = setInterval(() => {
-    //   console.log("refreshed");
+      //   console.log("refreshed");
       //Fetch the machine learning model ressults from thingspeak
       fetch("https://api.thingspeak.com/channels/1958878/fields/3.json?minutes")
         .then((response) => response.json())
@@ -61,89 +62,86 @@ const GraphBoard = () => {
 
   //choose which graph to display
   useEffect(() => {
-   console.log(soilcheckox+" "+raincheckox+""+tempcheckox);
-   if(!soilcheckox&&!raincheckox&&!tempcheckox)
-   {
-    setMode(0);
-    console.log("Data not selected");
-   }
-   else
-   if(soilcheckox&&!raincheckox&&!tempcheckox)
-   {
-    setMode(4);
-    setLineData(soilData);
-   }
-   else
-   if(!soilcheckox&&raincheckox&&!tempcheckox)
-   {
-    setMode(4);
-    setLineData(rainData);
-   }
-   else
-   if(!soilcheckox&&!raincheckox&&tempcheckox)
-   {
-    setMode(4);
-    setLineData(tempData);
-   }
-   else
-   if(!soilcheckox&& raincheckox&&tempcheckox)
-   {
-    setMode(1);
-    setLineData(allData);
-   }
-   else 
-   if(soilcheckox&& !raincheckox && tempcheckox)
-   {
-    setMode(2);
-    setLineData(allData);
-   }
-   else
-   if(soilcheckox&& raincheckox && !tempcheckox)
-   {
-    setMode(3);
-    setLineData(allData);
-   }
-   else
-   if(soilcheckox&& raincheckox && tempcheckox)
-   {
-    setMode(5);
-    setLineData(allData);
-   }
-   
-  }, [soilcheckox,raincheckox,tempcheckox]);
+    if (!soilcheckox && !raincheckox && !tempcheckox) {
+      setMode(0);
+      console.log("Data not selected");
+    } else if (soilcheckox && !raincheckox && !tempcheckox) {
+      setMode(4);
+      setLineData(soilData);
+    } else if (!soilcheckox && raincheckox && !tempcheckox) {
+      setMode(4);
+      setLineData(rainData);
+    } else if (!soilcheckox && !raincheckox && tempcheckox) {
+      setMode(4);
+      setLineData(tempData);
+    } else if (!soilcheckox && raincheckox && tempcheckox) {
+      setMode(1);
+      setLineData(allData);
+    } else if (soilcheckox && !raincheckox && tempcheckox) {
+      setMode(2);
+      setLineData(allData);
+    } else if (soilcheckox && raincheckox && !tempcheckox) {
+      setMode(3);
+      setLineData(allData);
+    } else if (soilcheckox && raincheckox && tempcheckox) {
+      setMode(5);
+      setLineData(allData);
+    }
+  }, [soilcheckox, raincheckox, tempcheckox,allData]);
 
-  useEffect(()=>
-    {
-        console.log("mode"+mode)
-
-    },[mode]
-  );
- 
 
   const handleCheckboxChange = (event) => {
     const { name, checked } = event.target;
-    console.log(name+" "+checked);
-    if(name == "SoilMoisture")
-    {
-        setSoilCheckbox(!soilcheckox);
+    console.log(name + " " + checked);
+    if (name == "SoilMoisture") {
+      setSoilCheckbox(!soilcheckox);
+    } else if (name == "Temperature") {
+      setTempCheckbox(!tempcheckox);
+    } else if (name == "Rainfall") {
+      setRainCheckbox(!raincheckox);
     }
-    else if(name == "Temperature")
-    {
-        setTempCheckbox(!tempcheckox);
-    }
-    else if(name == "Rainfall")
-    {
-        setRainCheckbox(!raincheckox);
-    }
-   
-    
-  }
+  };
+
+  //custom timerange
+  useEffect(() => {
+    //fetches historical data from thingspeak for graphs
+    console.log(timeRange);
+    fetch(link +"results="+ timeRange)
+      .then((response) => response.json())
+      .then((info) => {
+        const data = convData(info);
+        setSoilData(data[0]);
+        setTempData(data[1]);
+        setRainData(data[2]);
+        setAllData(data[3]);
+      });
+  }, [timeRange]);
+
+  const handleTimeRangeChange = (event) => {
+    let value = event.target.value;
+   setTimeRange(value);
+
+  };
+
+  // useEffect(() => {
+  //   console.log("refreshed");
+  //   //Fetch the machine learning model ressults from thingspeak
+  //   fetch("https://api.thingspeak.com/channels/1958878/fields/3.json?minutes")
+  //     .then((response) => response.json())
+  //     .then((info) => {
+  //       const data = convData(info, -1);
+  //       setWaterData(data);
+  //     });
+  // }, [refresh]);
 
   return (
     <>
       <h1 class="font-bold text-3xl mt-5 mx-3 justify-center items-center text-center">
         Graph Information
       </h1>
+      <p className="mt-5 mx-3 justify-center items-center text-center">
+        Select the data to see its graph
+      </p>
       <div className="mt-4 mx-1 grid md:grid-cols-10">
         {/* first card */}
         <div className="card col-span-10 lg:col-span-7 m-2">
@@ -152,9 +150,11 @@ const GraphBoard = () => {
 
             <div class="flex ...">
               <div class="flex-auto w-32 h-96">
-               {(mode==5 && <StackedAreaChart data={allData} />)} 
-               {(mode==4 && <LineChartz data={lineData} />)}
-               {(mode==1||mode==2||mode==3) && <BiaxialChart data={lineData} mode={mode} />} 
+                {mode == 5 && <StackedAreaChart data={allData} />}
+                {mode == 4 && <LineChartz data={lineData} />}
+                {(mode == 1 || mode == 2 || mode == 3) && (
+                  <BiaxialChart data={lineData} mode={mode} />
+                )}
               </div>
             </div>
 
@@ -172,23 +172,55 @@ const GraphBoard = () => {
             <h1 className="card-title justify-center">Graph Information</h1>
 
             <div className="divider"></div>
-                            
+
             <div className="form-control">
               <label className="label cursor-pointer justify-start">
-                <input type="checkbox" name ="SoilMoisture" onChange={handleCheckboxChange} unchecked className="checkbox" />
+                <input
+                  type="checkbox"
+                  name="SoilMoisture"
+                  onChange={handleCheckboxChange}
+                  unchecked
+                  className="checkbox"
+                />
                 <span className="label-text ml-2">Soil Moisture: </span>
               </label>
               <label className="label cursor-pointer justify-start">
-                <input type="checkbox" name ="Temperature" onChange={handleCheckboxChange} unchecked className="checkbox" />
+                <input
+                  type="checkbox"
+                  name="Temperature"
+                  onChange={handleCheckboxChange}
+                  unchecked
+                  className="checkbox"
+                />
                 <span className="label-text ml-2">Temperature: </span>
               </label>
               <label className="label cursor-pointer justify-start">
-                <input type="checkbox" name ="Rainfall" onChange={handleCheckboxChange} unchecked className="checkbox" />
+                <input
+                  type="checkbox"
+                  name="Rainfall"
+                  onChange={handleCheckboxChange}
+                  unchecked
+                  className="checkbox"
+                />
                 <span className="label-text ml-2">Rainfall: </span>
               </label>
             </div>
 
             <div className="divider"></div>
+
+            <select value={value} onChange={handleTimeRangeChange} className="select select-bordered w-full max-w-xs">
+              <option disabled selected>
+                Select a time range
+              </option>
+              <option value="7">Last Week</option>
+              <option selected value="30">Last Month</option>
+              <option value="90">Last 3 Months</option>
+              <option value="180">Last 6 Months</option>
+              <option value="365">Last Year</option>
+              {/* <option>Custom Time Range</option> */}
+            </select>
+
+
 
             {/* table */}
             {/* <div className="overflow-x-auto">
