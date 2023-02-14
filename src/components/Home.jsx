ae611ca9c95b8d8b18ddddc6e0434f79ac9e0ff9
Meghana <meganraj251@gmail.com>
2023-02-14 12:43:52 +0530

    Change Home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   <div className="hero-content text-center">
     <div className="max-w-md">
       <h1 className="text-5xl font-bold">Welcome!</h1>
-      <p className="py-6">This is a smart irrigation system. Add some more flowery text here</p>
+      <p className="py-6">This is a smart irrigation system</p>
       <button className="btn btn-primary" onClick={()=> window.location.href = "dashboard"}>Get Started</button>
     </div>
   </div>
